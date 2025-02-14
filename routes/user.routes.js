fd0b2f8c75413b282feea3eeb4d84aca47f3c11b
keiredin <se.keiredin.aman@gmail.com>
2025-02-14 23:31:43 +0300

    add Arcjet middleware for rate limiting and bot detection; update environment variables and user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,14 +1,12 @@
 import { Router } from "express";
+import { getUsers, getUser } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
-  res.send({ title: "GET all users" });
-});
+userRouter.get("/", getUsers);
 
-userRouter.get("/:id", (req, res) => {
-  res.send({ title: "GET user by id" });
-});
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => {
   res.send({ title: "CREATE new user" });
